9beba9797f8ba8e22ed7c97e8f132ca924f019db
Syed Mohammad Ali Raza Naqvi <razanaqvi2004@gmail.com>
2023-06-13 10:18:26 +0500

    populated the news articles using API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,13 +3,19 @@ import React from "react";
 const NewsItem = (props) => {
   return (
     <div className="card my-3" style={{ width: "18rem" }}>
-      <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
         {props.name}
       </span>
       <img src={props.URLToImage} className="card-img-top" alt="..." />
-      <div className="card-body d-flex flex-column align-items-center justify-content-center">
+      <div className="card-body d-flex flex-column justify-content-center">
         <h5 className="card-title">{props.title}</h5>
+        <p className="fw-light">{Date(props.publishedAt)}</p>
         <p className="card-text">{props.description}</p>
+        <p className="card-text">
+          <small className="text-body-secondary">
+            Author: {props.author ? props.author : "Anonymous"}
+          </small>
+        </p>
         <a
           href={props.URL}
           target="_blank"
